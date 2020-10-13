83acd91bcad0db388aa51e67d39eea251415ae65
LDprg <lukas_4dr@gmx.at>
2020-10-13 08:11:52 +0200

    remove old
    remove old implementation

--- test/main.cpp
+++ test/main.cpp
@@ -2,41 +2,19 @@
 
 #include <SFGL/loadShader.hpp>
 #include <SFGL/window.hpp>
-#include <SFGL/Triangel.hpp>
-#include <SFGL/Rectangel.hpp>
 
 using namespace sfgl;
-
-std::vector<RectangelData> Rectangels;
 
 Window window;
 
 int main()
 {
 	window.Create();
-	
-	RectangelData rectangel;
-
-	rectangel.EdgePos[0][0] = 1.f;
-	rectangel.EdgePos[0][1] = 1.f;
-	rectangel.EdgePos[1][0] = 0.f;
-	rectangel.EdgePos[1][1] = 0.f;
-
-	rectangel.EdgeColor[0] = 0.f;
-	rectangel.EdgeColor[1] = 1.f;
-	rectangel.EdgeColor[2] = 0.f;
-
-	Rectangels.emplace_back(rectangel);
-
-	Rectangel::Create(Rectangels);
-
-	Rectangel::Update(Rectangels);
 
 	do
 	{
 		window.ClearScreen();
 
-		Rectangel::Draw(Rectangels);
 
 		window.Draw();
 
@@ -45,8 +23,6 @@ int main()
 			window.ShouldClose(1);
 		}
 	} while (!window.ShouldClose());
-
-	Rectangel::Clean(Rectangels);
 
 	window.Clean();
 
